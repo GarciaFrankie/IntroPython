549421178264827e636216f45643cfb69309a43a
Frankie Gabriel Garcia <frankiegarcia@Frankies-MacBook-Pro.local>
2022-09-14 21:41:07 -0400

    hello.py added

---
+++ hello_world.py
@@ -0,0 +1,10 @@
+import numpy
+message = 'Hello World!'
+pad = 3
+n = len(message) + 2 * pad + 2
+print('*' * n)
+print('*' + ' ' * (n - 2) + '*')
+print('*' + ' ' * pad + message + ' ' * pad + '*')
+print('*' + ' ' * (n - 2) + '*')
+print('*' * n)
+
